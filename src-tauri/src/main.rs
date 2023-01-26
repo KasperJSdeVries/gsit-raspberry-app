2b61c0851547ddadf7c8be9b0fb7d4d882f95a94
Kasper de Vries <89792927+KasperJSdeVries@users.noreply.github.com>
2023-01-26 03:18:30 +0100

    Fixes everything

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,56 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+//#![cfg_attr(
+//    all(not(debug_assertions), target_os = "windows"),
+//    windows_subsystem = "windows"
+//)]
 
 use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::{Gpio, OutputPin};
+use tauri::Window;
+
+const LEDS: &[u8] = &[17, 27, 22];
+const MOTORS: &[u8] = &[23, 24, 25];
+
+struct DrawerControls {
+    leds: Vec<OutputPin>,
+    motors: Vec<OutputPin>,
+}
+
+impl DrawerControls {
+    fn new() -> Self {
+        let gpio = Gpio::new().unwrap();
+
+        let mut leds = Vec::new();
+        let mut motors = Vec::new();
+
+        for &led in LEDS {
+            let mut led = gpio.get(led).unwrap().into_output();
+            led.set_low();
+            leds.push(led);
+        }
+        for &motor in MOTORS {
+            let mut motor = gpio.get(motor).unwrap().into_output();
+            motor.set_low();
+            motors.push(motor);
+        }
+
+        Self { leds, motors }
+    }
+
+    fn open<'a>(&'a mut self, idx: usize) {
+        let led = &mut self.leds[idx % 3];
+        let motor = &mut self.motors[idx % 3];
+
+        led.set_high();
+        motor.set_high();
+        thread::sleep(Duration::from_secs(1));
+        motor.set_low();
+        thread::sleep(Duration::from_secs(5));
+        led.set_low();
+    }
+}
 
 const QUESTIONS: &[&str] = &[
     "Is your device from the Apple brand?",
@@ -14,6 +61,8 @@ struct Questionaire {
     questions: Vec<&'static str>,
     answers: Mutex<Vec<bool>>,
     index: Mutex<usize>,
+    drawers: Mutex<DrawerControls>,
+    drawer_idx: Mutex<usize>,
 }
 
 impl Questionaire {
@@ -22,6 +71,8 @@ impl Questionaire {
             questions: QUESTIONS.to_vec(),
             answers: Mutex::new(Vec::with_capacity(QUESTIONS.len())),
             index: Mutex::new(QUESTIONS.len()),
+            drawers: Mutex::new(DrawerControls::new()),
+            drawer_idx: Mutex::new(0),
         }
     }
 }
@@ -36,11 +87,30 @@ fn answer(value: bool, questionaire: tauri::State<Questionaire>) {
         .insert(*questionaire.index.lock().unwrap(), value);
 }
 
+#[derive(serde::Serialize, Clone)]
+struct FinishedQuestionairePayload {
+    drawer_idx: usize,
+}
+
 #[tauri::command]
 // get next question from DB? / maybe hard-coded?
-fn next_question(questionaire: tauri::State<'_, Questionaire>) -> String {
+fn next_question(questionaire: tauri::State<'_, Questionaire>, window: Window) -> String {
     *questionaire.index.lock().unwrap() += 1;
     if *questionaire.index.lock().unwrap() > questionaire.questions.len() - 1 {
+        window
+            .emit(
+                "finished-questionaire",
+                FinishedQuestionairePayload {
+                    drawer_idx: *questionaire.drawer_idx.lock().unwrap(),
+                },
+            )
+            .unwrap();
+        questionaire
+            .drawers
+            .lock()
+            .unwrap()
+            .open(*questionaire.drawer_idx.lock().unwrap() % 3);
+        *questionaire.drawer_idx.lock().unwrap() += 1;
         *questionaire.index.lock().unwrap() = 0;
     }
     questionaire.questions[*questionaire.index.lock().unwrap()].to_owned()
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import {invoke} from "@tauri-apps/api/tauri";
 
+let finishedQuestionaire = false;
 let questionEl: HTMLParagraphElement | null;
+let leftButton: HTMLButtonElement | null;
+let rightButton: HTMLButtonElement | null;
 
 async function give_answer(answer: boolean) {
+    if (finishedQuestionaire) {
+        if (leftButton) {
+            leftButton.textContent = "Yes";
+        }
+        if (rightButton) {
+            rightButton.hidden = false;
+        }
+        finishedQuestionaire = false;
+        return;
+    }
     await invoke("answer", {
         value: answer,
     });
     await update_question();
-
 }
 
 async function update_question() {
@@ -16,8 +28,23 @@ async function update_question() {
     }
 }
 
+window.addEventListener("finished-questionaire", () => {
+    finishedQuestionaire = true;
+    if (questionEl) {
+        questionEl.textContent = "Thank you for finishing the questionaire!";
+    }
+    if (leftButton) {
+        leftButton.textContent = "Restart questionaire";
+    }
+    if (rightButton) {
+        rightButton.hidden = true;
+    }
+})
+
 window.addEventListener("DOMContentLoaded", async () => {
     questionEl = document.querySelector("#question-paragraph");
+    leftButton = document.querySelector("#yes-button")
+    rightButton = document.querySelector("#no-button")
     await update_question();
     document
         .querySelector("#yes-button")
